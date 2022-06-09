44aa478c68fb51156f03a1980d88dc3939c29233
r1vvy <eseflers@gmail.com>
2022-06-09 22:38:16 +0300

    Add: recipe cook button css

--- src/components/recipesList/RecipesList.js
+++ src/components/recipesList/RecipesList.js
@@ -8,15 +8,17 @@ const RecipesList = ({ recipes, title }) => {
       {recipes.map((recipe) => (
         <div className="recipe-preview" key={recipe.id}>
           <h2 className="recipe-title"> {recipe.title} </h2>
-          <h3 className="recipe-time"> {recipe.cookingTime} </h3>
+          <h3 className="recipe-time"> {recipe.cookingTime} to make. </h3>
           <p className="recipe-method">
             {recipe.method.length > 100
               ? `${recipe.method.substring(0, 100)}...`
               : recipe.method}
           </p>
-          <Link className="recipe-cook" to={`/recipes/${recipe.id}`}>
-            <button>Cook this</button>
+          <div className="recipe-button">
+          <Link to={`/recipes/${recipe.id}`}>
+            <button className="recipe">Cook this</button>
           </Link>
+          </div>
         </div>
       ))}
     </div>
