0d1649e27eee7ec843579fa89c79e71763875c19
r1vvy <eseflers@gmail.com>
2022-06-09 21:42:27 +0300

    Add: recipe-list grid responsive design

--- src/components/recipesList/RecipesList.js
+++ src/components/recipesList/RecipesList.js
@@ -1,16 +1,21 @@
 import { Link } from "react-router-dom";
 
-import "./RecipesList.css"
+import "./RecipesList.css";
 
 const RecipesList = ({ recipes, title }) => {
   return (
     <div className="recipe-list">
       {recipes.map((recipe) => (
         <div className="recipe-preview" key={recipe.id}>
-          <Link to={`/recipes/${recipe.id}`}>
-            <h2> {recipe.title} </h2>
-            <h3> {recipe.cookingTime} </h3>
-            <p> {recipe.method} </p>
+          <h2 className="recipe-title"> {recipe.title} </h2>
+          <h3 className="recipe-time"> {recipe.cookingTime} </h3>
+          <p className="recipe-method">
+            {recipe.method.length > 100
+              ? `${recipe.method.substring(0, 100)}...`
+              : recipe.method}
+          </p>
+          <Link className="recipe-cook" to={`/recipes/${recipe.id}`}>
+            <button>Cook this</button>
           </Link>
         </div>
       ))}
@@ -18,4 +23,4 @@ const RecipesList = ({ recipes, title }) => {
   );
 };
 
-export default RecipesList
+export default RecipesList;
